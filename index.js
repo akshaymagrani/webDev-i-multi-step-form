cc7882177e4d47aa91e6068be1615ee15e5fdc1d
akshay.magrani <akshaymagrani1@gmail.com>
2023-11-07 23:34:52 +0530

    tried 2 convrt 'mo'/'yr' on toggle click. Pending.

--- index.js
+++ index.js
@@ -15,85 +15,12 @@ let back4 = document.getElementById("back4");
 
 let btnNav = document.getElementsByClassName("btn-nav");
 
-btnNav[0].addEventListener('click', display_card1);
-btnNav[1].addEventListener('click', display_card2);
-btnNav[2].addEventListener('click', display_card3);
-btnNav[3].addEventListener('click', display_card4);
+let M = "monthly";
+let Y = "yearly";
+let m = "mo";
+let y = "yr";
+let mY = document.getElementsByClassName("m-y");
 
-function display_card1() {
-    card2[0].classList.add("d-none");
-    card3[0].classList.add("d-none");
-    card4[0].classList.add("d-none");
-    card5[0].classList.add("d-none");
-    card1[0].classList.remove("d-none");
-    btnNav[0].classList.add("bg-blue");
-    btnNav[1].classList.remove("bg-blue");
-    btnNav[2].classList.remove("bg-blue");
-    btnNav[3].classList.remove("bg-blue");
-}
-display_card1();
-function display_card2() {
-    card1[0].classList.add("d-none");
-    card2[0].classList.remove("d-none");
-    card3[0].classList.add("d-none");
-    card4[0].classList.add("d-none");
-    card5[0].classList.add("d-none");
-    btnNav[0].classList.remove("bg-blue");
-    btnNav[1].classList.add("bg-blue");
-    btnNav[2].classList.remove("bg-blue");
-    btnNav[3].classList.remove("bg-blue");
-}
-function display_card3() {
-    card1[0].classList.add("d-none");
-    card2[0].classList.add("d-none");
-    card3[0].classList.remove("d-none");
-    card4[0].classList.add("d-none");
-    card5[0].classList.add("d-none");
-    btnNav[0].classList.remove("bg-blue");
-    btnNav[1].classList.remove("bg-blue");
-    btnNav[2].classList.add("bg-blue");
-    btnNav[3].classList.remove("bg-blue");
-}
-function display_card4() {
-    card1[0].classList.add("d-none");
-    card2[0].classList.add("d-none");
-    card3[0].classList.add("d-none");
-    card4[0].classList.remove("d-none");
-    card5[0].classList.add("d-none");
-    btnNav[0].classList.remove("bg-blue");
-    btnNav[1].classList.remove("bg-blue");
-    btnNav[2].classList.remove("bg-blue");
-    btnNav[3].classList.add("bg-blue");
-}
-function display_card5() {
-    card1[0].classList.add("d-none");
-    card3[0].classList.add("d-none");
-    card2[0].classList.add("d-none");
-    card4[0].classList.add("d-none");
-    card5[0].classList.remove("d-none");
-}
-
-next1.addEventListener('click', display_card2);
-next2.addEventListener('click', display_card3);
-next3.addEventListener('click', display_card4);
-next4.addEventListener('click', display_card5);
-
-back2.addEventListener('click', display_card1);
-back3.addEventListener('click', display_card2);
-back4.addEventListener('click', display_card3);
-
-function getSelectedRadio() {
-    let radioButtons = document.getElementsByName("Plan");
-    for (let radio of radioButtons) {
-        if (radio.checked) {
-            // radio.parentElement.style.border = "2px solid blue";
-            console.log(radio.parentNode);
-        }
-    }
-}
-
-
-// let yearlycost = document.getElementsByClassName("yearlycost");
 
 function yearlyCost() {
     // let checkbox = document.getElementById("checkbox");
@@ -104,37 +31,45 @@ function yearlyCost() {
     let offer = document.getElementsByClassName("gr-offer");
     let addOnRates = document.getElementsByClassName("addOn-rates");
     let finishRates = document.getElementsByClassName("finish-rate");
+    
+    console.log(mY);
     checkbox.addEventListener('change', function () {
         if (checkbox.checked) {
-            monthlycost[0].innerHTML = "$90/yr";
-            monthlycost[1].innerHTML = "$120/yr";
-            monthlycost[2].innerHTML = "$150/yr";
+            monthlycost[0].innerHTML = "$90";
+            monthlycost[1].innerHTML = "$120";
+            monthlycost[2].innerHTML = "$150";
             offer[0].style.display = "block";
             offer[1].style.display = "block";
             offer[2].style.display = "block";
-            addOnRates[0].innerHTML = "+$10/yr";
-            addOnRates[1].innerHTML = "+$20/yr";
-            addOnRates[2].innerHTML = "+$20/yr";
+            addOnRates[0].innerHTML = "+$10";
+            addOnRates[1].innerHTML = "+$20";
+            addOnRates[2].innerHTML = "+$20";
             finishRates[0].innerHTML = "90";
             finishRates[1].innerHTML = "10";
             finishRates[2].innerHTML = "20";
             console.log("checked");
             console.log(monthlycost[0].innerHTML);
+            for(let i = 0; i < mY.length; i++){
+                mY[i].innerHTML = y;
+                console.log(`${i}`,y, mY.length);
+            }
         } else {
-            monthlycost[0].innerHTML = "$9/mo";
-            monthlycost[1].innerHTML = "$12/mo";
-            monthlycost[2].innerHTML = "$15/mo";
+            monthlycost[0].innerHTML = "$9";
+            monthlycost[1].innerHTML = "$12";
+            monthlycost[2].innerHTML = "$15";
             offer[0].style.display = "none";
             offer[1].style.display = "none";
             offer[2].style.display = "none";
-            addOnRates[0].innerHTML = "+$1/mo";
-            addOnRates[1].innerHTML = "+$2/mo";
-            addOnRates[2].innerHTML = "+$2/mo";
+            addOnRates[0].innerHTML = "+$1";
+            addOnRates[1].innerHTML = "+$2";
+            addOnRates[2].innerHTML = "+$2";
             finishRates[0].innerHTML = "9";
             finishRates[1].innerHTML = "1";
             finishRates[2].innerHTML = "2";
-            addOn[0].style.border = "2px solid grey";
             console.log(monthlycost[0].innerHTML);
+            for(let i = 0; i < mY.length; i++){
+                mY[i].innerHTML = m;
+            }
         }  
     });
 }
@@ -164,3 +99,87 @@ function yearlyCost() {
 document.addEventListener('DOMContentLoaded', function () {
     yearlyCost();
 });
+
+btnNav[0].addEventListener('click', display_card1);
+btnNav[1].addEventListener('click', display_card2);
+btnNav[2].addEventListener('click', display_card3);
+btnNav[3].addEventListener('click', display_card4);
+
+function display_card1() {
+    card2[0].classList.add("d-none");
+    card3[0].classList.add("d-none");
+    card4[0].classList.add("d-none");
+    card5[0].classList.add("d-none");
+    card1[0].classList.remove("d-none");
+    btnNav[0].classList.add("bg-blue");
+    btnNav[1].classList.remove("bg-blue");
+    btnNav[2].classList.remove("bg-blue");
+    btnNav[3].classList.remove("bg-blue");
+}
+display_card1();
+function display_card2() {
+    card1[0].classList.add("d-none");
+    card2[0].classList.remove("d-none");
+    card3[0].classList.add("d-none");
+    card4[0].classList.add("d-none");
+    card5[0].classList.add("d-none");
+    btnNav[0].classList.remove("bg-blue");
+    btnNav[1].classList.add("bg-blue");
+    btnNav[2].classList.remove("bg-blue");
+    btnNav[3].classList.remove("bg-blue");
+    yearlyCost()
+}
+function display_card3() {
+    card1[0].classList.add("d-none");
+    card2[0].classList.add("d-none");
+    card3[0].classList.remove("d-none");
+    card4[0].classList.add("d-none");
+    card5[0].classList.add("d-none");
+    btnNav[0].classList.remove("bg-blue");
+    btnNav[1].classList.remove("bg-blue");
+    btnNav[2].classList.add("bg-blue");
+    btnNav[3].classList.remove("bg-blue");
+    yearlyCost()
+}
+function display_card4() {
+    card1[0].classList.add("d-none");
+    card2[0].classList.add("d-none");
+    card3[0].classList.add("d-none");
+    card4[0].classList.remove("d-none");
+    card5[0].classList.add("d-none");
+    btnNav[0].classList.remove("bg-blue");
+    btnNav[1].classList.remove("bg-blue");
+    btnNav[2].classList.remove("bg-blue");
+    btnNav[3].classList.add("bg-blue");
+    yearlyCost()
+}
+function display_card5() {
+    card1[0].classList.add("d-none");
+    card3[0].classList.add("d-none");
+    card2[0].classList.add("d-none");
+    card4[0].classList.add("d-none");
+    card5[0].classList.remove("d-none");
+    yearlyCost()
+}
+
+next1.addEventListener('click', display_card2);
+next2.addEventListener('click', display_card3);
+next3.addEventListener('click', display_card4);
+next4.addEventListener('click', display_card5);
+
+back2.addEventListener('click', display_card1);
+back3.addEventListener('click', display_card2);
+back4.addEventListener('click', display_card3);
+
+function getSelectedRadio() {
+    let radioButtons = document.getElementsByName("Plan");
+    for (let radio of radioButtons) {
+        if (radio.checked) {
+            // radio.parentElement.style.border = "2px solid blue";
+            console.log(radio.parentNode);
+        }
+    }
+}
+
+
+// let yearlycost = document.getElementsByClassName("yearlycost");
